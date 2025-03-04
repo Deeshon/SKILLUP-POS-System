56d1f044813ef1430166f0987e194ed7ded70fba
Deeshon Hunukumbura <deeshonhunukumbura18@gmail.com>
2025-03-05 02:13:46 +0530

    data bulk upload scripts

--- ts-backend/prisma/migrations/20250304185901_init_and_add_transactions_model/migration.sql
+++ ts-backend/prisma/migrations/20250304185901_init_and_add_transactions_model/migration.sql
@@ -1,6 +1,15 @@
 -- CreateEnum
 CREATE TYPE "Role" AS ENUM ('ADMIN', 'CASHIER');
 
+-- CreateEnum
+CREATE TYPE "Unit" AS ENUM ('KG', 'PC');
+
+-- CreateEnum
+CREATE TYPE "PaymentType" AS ENUM ('CASH', 'CARD', 'TRANSFER');
+
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('PENDING', 'COMPLETED', 'CANCELLED');
+
 -- CreateTable
 CREATE TABLE "Company" (
     "id" TEXT NOT NULL,
@@ -42,6 +51,58 @@ CREATE TABLE "Store" (
 );
 
 -- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Brand" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+
+    CONSTRAINT "Brand_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "storeId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "slu" TEXT NOT NULL,
+    "sku" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "quantity" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "unit" "Unit" NOT NULL,
+    "imageUrls" TEXT[],
+    "categoryId" TEXT NOT NULL,
+    "brandId" TEXT NOT NULL,
+    "manufacturedDate" TIMESTAMP(3) NOT NULL,
+    "expiryDate" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" TEXT NOT NULL,
+    "customerName" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    "total" DOUBLE PRECISION NOT NULL,
+    "paymentType" "PaymentType" NOT NULL,
+    "paymentRef" TEXT NOT NULL,
+    "status" "Status" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "_StoreToUser" (
     "A" TEXT NOT NULL,
     "B" TEXT NOT NULL,
@@ -69,6 +130,18 @@ ALTER TABLE "User" ADD CONSTRAINT "User_companyId_fkey" FOREIGN KEY ("companyId"
 
 -- AddForeignKey
 ALTER TABLE "Store" ADD CONSTRAINT "Store_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "Company"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_storeId_fkey" FOREIGN KEY ("storeId") REFERENCES "Store"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_brandId_fkey" FOREIGN KEY ("brandId") REFERENCES "Brand"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_StoreToUser" ADD CONSTRAINT "_StoreToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "Store"("id") ON DELETE CASCADE ON UPDATE CASCADE;
